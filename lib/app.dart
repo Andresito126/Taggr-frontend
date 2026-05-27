be43aa6ddda68957f838f3aa5abef7669c8c76d5
Andresito126 <159158192+Andresito126@users.noreply.github.com>
2026-05-27 16:21:43 -0600

    Add: little refactor

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:taggr/core/components/main_layout.dart';
+import 'package:taggr/core/navigation/main_layout.dart';
 import 'package:taggr/features/auth/presentation/screens/login_screen.dart';
 import 'package:taggr/features/auth/presentation/screens/register_screen.dart';
 import 'package:taggr/features/posts/presentation/screens/create_post_screen.dart';
