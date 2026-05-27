56123f113d1c12890a4035ee89cd8052c0976466
Andresito126 <159158192+Andresito126@users.noreply.github.com>
2026-05-27 04:20:04 -0600

    remove: import removed

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:taggr/shared/components/input_field.dart';
-import 'package:taggr/shared/components/title_section.dart';
 import 'package:taggr/shared/theme/app_colors.dart';
 import 'package:taggr/shared/theme/app_text_styles.dart';
 
