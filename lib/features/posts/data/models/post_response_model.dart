ce618356445767d2f21d7a24a6c4bbcbadeeef08
Andresito126 <159158192+Andresito126@users.noreply.github.com>
2026-06-01 01:42:26 -0600

    Add:get all posts

--- lib/features/posts/data/models/post_response_model.dart
+++ lib/features/posts/data/models/post_response_model.dart
@@ -10,14 +10,27 @@ class PostResponseModel extends Post {
     required super.tags,
   });
 
-  factory PostResponseModel.fromJson(Map<String, dynamic> json) {
+factory PostResponseModel.fromJson(Map<String, dynamic> json) {
+    List<String> parsedTags = [];
+
+    if (json['tags'] != null) {
+      if (json['tags'] is String) {
+        String rawTags = json['tags'].toString().replaceAll('{', '').replaceAll('}', '');
+        if (rawTags.trim().isNotEmpty) {
+          parsedTags = rawTags.split(',').map((e) => e.trim()).toList();
+        }
+      } else if (json['tags'] is Iterable) {
+        parsedTags = (json['tags'] as Iterable).map((e) => e.toString()).toList();
+      }
+    }
+
     return PostResponseModel(
-      id: json['id'],
-      userId: json['user_id'],
-      title: json['title'],
-      description: json['description'],
-      category: json['category'],
-      tags: List<String>.from(json['tags'] ?? []), 
+      id: json['id'].toString(),
+      userId: json['user_id']?.toString() ?? '',
+      title: json['title']?.toString() ?? 'Sin título',
+      description: json['description']?.toString() ?? '',
+      category: json['category']?.toString() ?? 'General',
+      tags: parsedTags,
     );
   }
 }
--- lib/features/posts/presentation/screens/home_screen.dart
+++ lib/features/posts/presentation/screens/home_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:taggr/features/posts/presentation/components/post_card.dart';
+import 'package:taggr/features/posts/presentation/providers/post_provider.dart';
+import 'package:taggr/features/posts/presentation/providers/post_ui_state.dart';
 import 'package:taggr/shared/components/title_section.dart';
 import 'package:taggr/shared/theme/app_colors.dart';
+import 'package:provider/provider.dart';
+import 'package:taggr/shared/theme/app_text_styles.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,6 +15,14 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<PostProvider>().loadPosts();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,32 +41,62 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       body: SafeArea(
-        child: SingleChildScrollView(
-          //          child: Padding(
-          //           padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            children: [
-              const SizedBox(height: 30),
-              PostCard(
-                imageUrl: "assets/imgs/graffiti_img.png",
-                category: "Graffiti",
-                userName: "AndreJu",
-                title: "Jungle",
-                description: "This is a graffit very fkn cool babe",
-                likes: 10,
-                comments: 20,
-              ),
-              PostCard(
-                imageUrl: "assets/imgs/tatto_img.png",
-                category: "Tatto",
-                userName: "AndreJu",
-                title: "Tatto",
-                description: "This is a graffit very fkn cool babe",
-                likes: 12,
-                comments: 18,
-              ),
-            ],
-          ),
+        child: Consumer<PostProvider>(
+          builder: (context, provider, child) {
+            //pone el state actual
+            final state = provider.state;
+
+            if (state is PostLoading || state is PostInitial) {
+              return const Center(
+                child: CircularProgressIndicator(color: AppColors.neonGreen),
+              );
+            }
+
+            if (state is PostError) {
+              return Center(
+                child: Text(
+                  "Error: ${state.message}",
+                  style: AppTextStyles.body.copyWith(color: Colors.redAccent),
+                ),
+              );
+            }
+
+            if (state is PostLoaded) {
+              final posts = state.posts;
+
+              if (posts.isEmpty) {
+                return Center(
+                  child: Text(
+                    "Aún no hay posts.",
+                    style: AppTextStyles.body.copyWith(
+                      color: AppColors.textSecondary,
+                    ),
+                  ),
+                );
+              }
+
+              return ListView.builder(
+                padding: const EdgeInsets.only(top: 30),
+                itemCount: posts.length,
+                itemBuilder: (context, index) {
+                  final post = posts[index];
+                  return PostCard(
+                    imageUrl: "assets/imgs/tatto_img.png",
+                    category: post.category,
+                    userName: post.userId.substring(0, 4),
+                    title: post.title,
+                    description: post.description,
+                    likes: 12,
+                    comments: 18,
+                  );
+                },
+              );
+            }
+            return const SizedBox.shrink();
+
+            //          child: Padding(
+            //           padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          },
         ),
       ),
       //     ),
